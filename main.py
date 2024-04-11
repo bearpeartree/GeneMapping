476f8076fa22ee1208324d2e632e8e0111c7bef8
Lisa <tranlisa@live.de>
2024-04-11 18:32:52 +0200

    Reading from text file completed

--- main.py
+++ main.py
@@ -5,14 +5,19 @@ def main():
     #A b C 5
     name_file = input("Please enter the name of your textfile: ")
     my_gametes = extract_data(name_file);
-
-
-    gametes_and_counts = {}
+    gametes_and_counts = save_into_dict(my_gametes)
+    print(gametes_and_counts)
 
 
 #And save it into dict
 def save_into_dict(gametes_list):
-    return None
+    gametes_dict = {}
+    for gamete in gametes_list:
+        gamete_count = separate_data(gamete)
+        gametes_dict[gamete_count[0]] = gamete_count[1]
+
+    return gametes_dict
+
 
 #Read the file
 #Return the list with each line
@@ -23,6 +28,15 @@ def extract_data(filename):
             gametes.append(line.strip())
 
     return gametes
+
+
+def separate_data(some_string):
+    g_c = []
+    for i in range(0, len(some_string)):
+        if some_string[i].isnumeric():
+            g_c.append(some_string[0:i].strip())
+            g_c.append(int(some_string[i:len(some_string)].strip()))
+            return g_c;
 
 
 if __name__ == "__main__":
